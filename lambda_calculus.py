f182996db4b4ff97c106c5e6fa958ce2cd24dc6d
Golichen <2531095701@qq.com>
2025-05-21 18:38:46 +0300

    Update lambda_calculus.py

--- lambda_calculus.py
+++ lambda_calculus.py
@@ -627,7 +627,7 @@ class Parser:
 
     def _parse_application(self) -> LambdaTerm:
         term: LambdaTerm
-        
+
         if self.current_token[0] == LAMBDA:
             term = self._parse_abstraction()
         else:
